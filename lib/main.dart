29151be1893890b9987a7de8367445fec1d24146
grizzlybe4r <124220038@student.upnyk.ac.id>
2024-09-18 20:09:34 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: use_super_parameters
+
+import 'package:flutter/material.dart';
+import 'package:tugas1_login/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
